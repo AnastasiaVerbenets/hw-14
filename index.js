ac5354cf4cf53e82771641c4b244150fd638102d
anastasia <anastasiaverbenets@gmail.com>
2024-10-07 17:08:42 +0300

    result

---
+++ index.js
@@ -0,0 +1,52 @@
+// 1
+// Порівняння кількох промісів
+
+// function delayedPromise(value, delay) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(value);
+//         }, delay);
+//     });
+// }
+
+// const promises = [
+//     delayedPromise('First', 1000),
+//     delayedPromise('Second', 1300),
+//     delayedPromise('Third', 2000),
+//     delayedPromise('Fourth', 3000),
+//     delayedPromise('Fifth', 2500)
+// ];
+
+// Promise.all(promises).then(results => {
+//     console.log("Results:");
+//     results.forEach((result, idx) => {
+//         console.log(`Promise ${idx + 1}: ${result}`)
+//     });
+// }).catch(error => { console.log(error) });
+
+// 2
+// "Змагання промісів"
+// Ви маєте набір функцій, які повертають проміси з випадковими затримками.Ваше завдання — виконати всі проміси одночасно за допомогою Promise.race і отримати результат найшвидшого проміса.
+
+// Використайте Promise.race, щоб виконати всі проміси з масиву і отримати результат найшвидшого проміса.
+// Обробіть результат найшвидшого проміса та виведіть його у консоль.
+
+function randomDelay(value) {
+    const delay = Math.floor(Math.random() * 4001) + 1000;
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(value);
+        }, delay);
+    });
+}
+
+const promises2 = [
+    randomDelay('Value 1'),
+    randomDelay('Value 2'),
+    randomDelay('Value 3'),
+    randomDelay('Value 4'),
+    randomDelay('Value 5')
+];
+
+Promise.race(promises2).then(result => { console.log('The fastest promise:', result) }).catch(error => { console.log(error) });
